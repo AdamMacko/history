2d35d0e61bcb4985a9b94d43ee6ecab206e13bd9
AdamMacko <mackoadam60@gmail.com>
2025-11-30 17:13:45 +0100

    remove old onepage directory

--- src/app/onepage/sections/Tools.tsx
+++
@@ -1,9 +0,0 @@
-import Section from "./Section";
-
-export default function Tools() {
-  return (
-    <Section id="tools" title="Vybavenie">
-      <p>Sem príde vybavenie</p>
-    </Section>
-  );
-}
--- src/app/onepage/sections/program.tsx
+++
@@ -1,9 +0,0 @@
-import Section from "./Section";
-
-export default function Program() {
-  return (
-    <Section id="program" title="Program">
-      <p>Sem príde program</p>
-    </Section>
-  );
-}
